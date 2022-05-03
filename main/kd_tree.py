91f53f873e4de076cca160f283ad69c7a74834c4
GitThisJoel <joelbacker10@gmail.com>
2022-05-03 11:44:43 +0200

    started work on the kd algorithm, changed value in Node to location

--- main/kd_tree.py
+++ main/kd_tree.py
@@ -7,15 +7,25 @@ Node = TypeVar('Node')
 
 @dataclass
 class Node:
-    value: int
+    location: tuple
     left: Node = None
     right: Node = None
 
-def create_kd_tree(points, depth):
-    return
+def kd_tree(points, depth):
+    k = len(points[0])
+    axis = depth % k
+
+    points.sort(key=lambda x: x[axis])
+    
+    median = len(points)//2
+    return Node(
+            location=points[median],
+            left=points[:median],
+            right=points[median+1:]
+        )
 
 if __name__ == "__main__":
-    a = Node(10)
-    b = Node(20)
-    c = Node(value=30, left=a, right=b)
+    a = Node((1,2))
+    b = Node((3,4))
+    c = Node(location=(5,6), left=a, right=b)
     print(c)

27a20312c03635857944bc0956ff324561a25b0c
GitThisJoel <joelbacker10@gmail.com>
2022-05-03 11:49:04 +0200

    found a typo in random points

--- main/random_points.py
+++ main/random_points.py
@@ -6,7 +6,7 @@ def random_points(n: int, dim: int = 2, ranges: [(Number, Number)] = None):
         ranges = [(0,1)] * dim
     elif len(ranges) < dim:
         print("WARNING: the number of ranges do not match to number of dimensions in gen_random_points.")
-        ranges += [rnages[-1]] * (dim - len(ranges))
+        ranges += [ranges[-1]] * (dim - len(ranges))
 
     points = [0] * n
     for i in range(n):
